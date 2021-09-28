6e5e082d8eb3a50b969acbc944b4b72391134028
Shahar Mintz <smintz@protoconf.com>
2021-09-29 01:04:08 +0300

    test: even more flagset coverage

--- flagset_test.go
+++ flagset_test.go
@@ -2,6 +2,7 @@ package libprotoconf
 
 import (
 	"flag"
+	"reflect"
 	"testing"
 
 	"google.golang.org/protobuf/proto"
@@ -18,9 +19,10 @@ func TestConfig_FlagSet(t *testing.T) {
 	}
 
 	tests := []struct {
-		name   string
-		fields fields
-		want   proto.Message
+		name    string
+		fields  fields
+		want    proto.Message
+		wantErr bool
 	}{
 		{
 			name: "apipb",
@@ -28,7 +30,8 @@ func TestConfig_FlagSet(t *testing.T) {
 				p:    &apipb.Api{},
 				args: []string{"-name", "libprotoconf", "-version", "v1", "-syntax", "SYNTAX_PROTO3"},
 			},
-			want: &apipb.Api{Name: "libprotoconf", Version: "v1", Syntax: typepb.Syntax_SYNTAX_PROTO3},
+			want:    &apipb.Api{Name: "libprotoconf", Version: "v1", Syntax: typepb.Syntax_SYNTAX_PROTO3},
+			wantErr: false,
 		},
 		{
 			name: "apipb_default_enum",
@@ -36,7 +39,17 @@ func TestConfig_FlagSet(t *testing.T) {
 				p:    &apipb.Api{},
 				args: []string{"-name", "libprotoconf", "-version", "v1", "-syntax", "SYNTAX_PROTO2"},
 			},
-			want: &apipb.Api{Name: "libprotoconf", Version: "v1"},
+			want:    &apipb.Api{Name: "libprotoconf", Version: "v1"},
+			wantErr: false,
+		},
+		{
+			name: "apipb_bad_enum",
+			fields: fields{
+				p:    &apipb.Api{},
+				args: []string{"-name", "libprotoconf", "-version", "v1", "-syntax", "SYNTAX_PROTO4"},
+			},
+			want:    &apipb.Api{Name: "libprotoconf", Version: "v1"},
+			wantErr: true,
 		},
 		{
 			name: "timestamppb",
@@ -44,7 +57,8 @@ func TestConfig_FlagSet(t *testing.T) {
 				p:    timestamppb.Now(),
 				args: []string{"-nanos", "123", "-seconds", "456"},
 			},
-			want: &timestamppb.Timestamp{Seconds: 456, Nanos: 123},
+			want:    &timestamppb.Timestamp{Seconds: 456, Nanos: 123},
+			wantErr: false,
 		},
 		{
 			name: "help",
@@ -52,23 +66,35 @@ func TestConfig_FlagSet(t *testing.T) {
 				p:    &apipb.Api{},
 				args: []string{"-h"},
 			},
-			want: &apipb.Api{},
+			want:    &apipb.Api{},
+			wantErr: true,
 		},
 		{
-			name: "bool_value_true",
+			name: "bool_true",
 			fields: fields{
 				p:    &wrapperspb.BoolValue{},
 				args: []string{"-value", "true"},
 			},
-			want: &wrapperspb.BoolValue{Value: true},
+			want:    &wrapperspb.BoolValue{Value: true},
+			wantErr: false,
 		},
 		{
-			name: "bool_value_false",
+			name: "bool_false",
 			fields: fields{
 				p:    &wrapperspb.BoolValue{},
 				args: []string{"-value=false"},
 			},
-			want: &wrapperspb.BoolValue{Value: false},
+			want:    &wrapperspb.BoolValue{Value: false},
+			wantErr: false,
+		},
+		{
+			name: "bad_bool",
+			fields: fields{
+				p:    &wrapperspb.BoolValue{},
+				args: []string{"-value=hello"},
+			},
+			want:    &wrapperspb.BoolValue{Value: false},
+			wantErr: true,
 		},
 		{
 			name: "uint32",
@@ -76,7 +102,26 @@ func TestConfig_FlagSet(t *testing.T) {
 				p:    &wrapperspb.UInt32Value{},
 				args: []string{"-value", "123"},
 			},
-			want: &wrapperspb.UInt32Value{Value: 123},
+			want:    &wrapperspb.UInt32Value{Value: 123},
+			wantErr: false,
+		},
+		{
+			name: "bad_uint32",
+			fields: fields{
+				p:    &wrapperspb.UInt32Value{},
+				args: []string{"-value", "abc"},
+			},
+			want:    &wrapperspb.UInt32Value{},
+			wantErr: true,
+		},
+		{
+			name: "bad_int32",
+			fields: fields{
+				p:    &wrapperspb.Int32Value{},
+				args: []string{"-value", "abc"},
+			},
+			want:    &wrapperspb.Int32Value{},
+			wantErr: true,
 		},
 		{
 			name: "uint64",
@@ -84,7 +129,26 @@ func TestConfig_FlagSet(t *testing.T) {
 				p:    &wrapperspb.UInt64Value{},
 				args: []string{"-value", "123"},
 			},
-			want: &wrapperspb.UInt64Value{Value: 123},
+			want:    &wrapperspb.UInt64Value{Value: 123},
+			wantErr: false,
+		},
+		{
+			name: "bad_uint64",
+			fields: fields{
+				p:    &wrapperspb.UInt64Value{},
+				args: []string{"-value", "hello"},
+			},
+			want:    &wrapperspb.UInt64Value{},
+			wantErr: true,
+		},
+		{
+			name: "bad_int64",
+			fields: fields{
+				p:    &wrapperspb.Int64Value{},
+				args: []string{"-value", "hello"},
+			},
+			want:    &wrapperspb.Int64Value{},
+			wantErr: true,
 		},
 		{
 			name: "float64",
@@ -92,7 +156,26 @@ func TestConfig_FlagSet(t *testing.T) {
 				p:    &wrapperspb.FloatValue{},
 				args: []string{"-value", "123.456"},
 			},
-			want: &wrapperspb.FloatValue{Value: 123.456},
+			want:    &wrapperspb.FloatValue{Value: 123.456},
+			wantErr: false,
+		},
+		{
+			name: "int_to_float64",
+			fields: fields{
+				p:    &wrapperspb.FloatValue{},
+				args: []string{"-value", "123"},
+			},
+			want:    &wrapperspb.FloatValue{Value: 123},
+			wantErr: false,
+		},
+		{
+			name: "bad_float64",
+			fields: fields{
+				p:    &wrapperspb.FloatValue{},
+				args: []string{"-value", "abc"},
+			},
+			want:    &wrapperspb.FloatValue{},
+			wantErr: true,
 		},
 		{
 			name: "double",
@@ -100,7 +183,8 @@ func TestConfig_FlagSet(t *testing.T) {
 				p:    &wrapperspb.DoubleValue{},
 				args: []string{"-value", "123.456"},
 			},
-			want: &wrapperspb.DoubleValue{Value: 123.456},
+			want:    &wrapperspb.DoubleValue{Value: 123.456},
+			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
@@ -110,8 +194,39 @@ func TestConfig_FlagSet(t *testing.T) {
 			c.SetLogger(c.Logger.WithName(t.Name()))
 			fs := flag.NewFlagSet(tt.name, flag.ContinueOnError)
 			c.PopulateFlagSet(fs)
-			if fs.Parse(tt.fields.args); !proto.Equal(tt.fields.p, tt.want) {
+			if err := fs.Parse(tt.fields.args); (err != nil) != tt.wantErr {
+				t.Errorf("Config.FlagSet() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !proto.Equal(tt.fields.p, tt.want) {
 				t.Errorf("Config.FlagSet() = %v, want %v", tt.fields.p, tt.want)
+			}
+		})
+	}
+}
+
+func TestConfig_DefaultFlagSet(t *testing.T) {
+	type fields struct {
+		msg proto.Message
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "test",
+			fields: fields{
+				msg: &apipb.Api{},
+			},
+			want: "google.protobuf.Api",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewConfig(tt.fields.msg)
+			fs := c.DefaultFlagSet()
+			if got := fs.Name(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Config.DefaultFlagSet() = %v, want %v", got, tt.want)
 			}
 		})
 	}
