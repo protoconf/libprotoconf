3cf310671421891e32664184ac222d17aa357575
Shahar Mintz <smintz@protoconf.com>
2021-09-29 01:52:22 +0300

    test: more coverage path detectors

--- libprotoconf_test.go
+++ libprotoconf_test.go
@@ -1,6 +1,7 @@
 package libprotoconf
 
 import (
+	"os"
 	"reflect"
 	"sort"
 	"testing"
@@ -72,6 +73,110 @@ func TestConfig_DetectWorkspace(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("Config.DetectWorkspace() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestConfig_LoadFromWorkspace(t *testing.T) {
+	type fields struct {
+		msg proto.Message
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name:    "test",
+			fields:  fields{msg: &apipb.Api{}},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewConfig(tt.fields.msg)
+			if err := c.LoadFromWorkspace(); (err != nil) != tt.wantErr {
+				t.Errorf("Config.LoadFromWorkspace() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestConfig_LoadFromUserDir(t *testing.T) {
+	type fields struct {
+		msg  proto.Message
+		home string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []string
+		wantErr bool
+	}{
+		{
+			name: "test",
+			fields: fields{
+				msg:  &apipb.Api{},
+				home: "/tmp",
+			},
+			want:    []string{"/tmp/.google", "/tmp/.google/protobuf"},
+			wantErr: false,
+		},
+		{
+			name: "empty_home",
+			fields: fields{
+				msg:  &apipb.Api{},
+				home: "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Setenv("HOME", tt.fields.home)
+			c := NewConfig(tt.fields.msg)
+			if err := c.LoadFromUserDir(); (err != nil) != tt.wantErr {
+				t.Errorf("Config.LoadFromUserDir() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			sort.Strings(c.config.ConfigDirs)
+			sort.Strings(tt.want)
+			if !reflect.DeepEqual(c.config.ConfigDirs, tt.want) {
+				t.Errorf("Config.LoadFromUserDir() =  %v, %v", c.config.ConfigDirs, tt.want)
+			}
+		})
+	}
+}
+
+func TestConfig_LoadFromDefaultDirs(t *testing.T) {
+	type fields struct {
+		msg  proto.Message
+		home string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []string
+	}{
+		{
+			name: "test",
+			fields: fields{
+				msg:  &apipb.Api{},
+				home: "/tmp",
+			},
+			want: []string{"/etc/google", "/etc/google/protobuf", "/tmp/.google", "/tmp/.google/protobuf"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewConfig(tt.fields.msg)
+			os.Setenv("HOME", tt.fields.home)
+			c.LoadFromDefaultDirs()
+			sort.Strings(c.config.ConfigDirs)
+			sort.Strings(tt.want)
+			if !reflect.DeepEqual(c.config.ConfigDirs, tt.want) {
+				t.Errorf("Config.LoadFromDefaultDirs() =  %v, %v", c.config.ConfigDirs, tt.want)
 			}
 		})
 	}
